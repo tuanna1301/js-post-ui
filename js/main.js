236a10be85e5aac526323e1b8271c146c40daefa
tuanna1301 <59677260+tuanna1301@users.noreply.github.com>
2022-03-31 16:29:48 +0700

    Setup API file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log("hello my friend");
+import postApi from './api/postApi'
+
+async function main() {
+  const queryParams = {
+    _page: 1,
+    limit: 10,
+  }
+  const respone = await postApi.getAll(queryParams)
+  console.log(respone)
+}
+
+main()
